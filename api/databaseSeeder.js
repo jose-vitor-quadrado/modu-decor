864737bb1c9f54e30e87801eb45109276b6fde72
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2024-11-11 19:59:42 -0300

    Add model Order

--- api/databaseSeeder.js
+++ api/databaseSeeder.js
@@ -1,19 +1,24 @@
 const router = require("express").Router();
+const AsyncHandler = require("express-async-handler");
 const User = require("./models/User");
 const users = require("./data/Users");
 const Product = require("./models/Product");
 const products = require("./data/Products");
 
-router.post("/users", async (req, res) => {
-  await User.deleteMany({});
-  const UserSeeder = await User.insertMany(users);
-  res.send({ UserSeeder });
-});
+router.post("/users", AsyncHandler(
+  async (req, res) => {
+    await User.deleteMany({});
+    const UserSeeder = await User.insertMany(users);
+    res.send({ UserSeeder });
+  }
+));
 
-router.post("/products", async (req, res) => {
-  await Product.deleteMany({});
-  const ProductSeeder = await Product.insertMany(products);
-  res.send({ ProductSeeder });
-});
+router.post("/products", AsyncHandler(
+  async (req, res) => {
+    await Product.deleteMany({});
+    const ProductSeeder = await Product.insertMany(products);
+    res.send({ ProductSeeder });
+  }
+));
 
 module.exports = router;
