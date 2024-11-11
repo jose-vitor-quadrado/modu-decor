bf7435cce1285bc5d1d0ff52039dc3e1bec4de66
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2024-11-11 15:21:00 -0300

    Add data/Products.js, data/Users.js and models/Product.js, models/User.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+const products = require("./data/Products");
+const mongoose = require("mongoose");
+
+const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+// api product test route
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find((product) => product.id === req.params.id);
+//   res.json(product);
+// });
+
+// connect db
+mongoose.connect(process.env.MONGOOSEDB_URL)
+  .then(() => console.log("db connected"))
+  .then((err) => {
+    err;
+  });
+
 app.listen(PORT || 9000, () => {
-  console.log("server listening on port 9000")
+  console.log(`server listening on port ${PORT}`);
 });
