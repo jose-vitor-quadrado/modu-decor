90ff40c4d3f02aecea5eff863430c6f94ffbd545
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2024-11-11 22:31:38 -0300

    Add put method in User route

--- api/routes/User.js
+++ api/routes/User.js
@@ -56,7 +56,7 @@ userRoute.post("/", AsyncHandler(async (req, res) => {
   }
 }));
 
-// profile data
+// get auth profile data
 userRoute.get(
   "/profile",
   protect,
@@ -69,6 +69,34 @@ userRoute.get(
         email: user.email,
         isAdmin: user.isAdmin,
         createdAt: user.createdAt,
+      });
+    }
+    else {
+      res.status(404);
+      throw new Error("USER NOT FOUND");
+    }
+  })
+);
+
+// user profile update
+userRoute.put("/profile",
+  protect,
+  AsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+      const updatedUser = await user.save();
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        createdAt: updatedUser.createdAt,
+        token: generateToken(updatedUser._id)
       });
     }
     else {
