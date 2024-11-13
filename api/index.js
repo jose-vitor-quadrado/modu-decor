7db7dd7e0003ef331840d009c410f7a9625bad55
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2024-11-13 14:30:35 -0300

    Remove tester routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const products = require("./data/Products");
 const databaseSeeder = require("./databaseSeeder");
 const userRoute = require("./routes/User");
 const productRoute = require("./routes/Product");
@@ -12,15 +11,6 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
-
-// api product test route
-// app.get("/api/products", (req, res) => {
-//   res.json(products);
-// });
-// app.get("/api/products/:id", (req, res) => {
-//   const product = products.find((product) => product.id === req.params.id);
-//   res.json(product);
-// });
 
 mongoose.connect(process.env.MONGOOSEDB_URL)
   .then(() => console.log("db connected"))
